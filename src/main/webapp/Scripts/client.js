94733e4e866022ad0699d74bd92f6a7304456775
Vít Svoboda <vit.svoboda@gmail.com>
2014-03-21 15:19:28 +0100

    Refactoring - used require.js
    Script modules rewritten to require.js modules. This should help later
    on with clean javascript application design.

--- src/main/webapp/Scripts/client.js
+++ src/main/webapp/Scripts/client.js
@@ -1,36 +1,43 @@
-$(document).ready(function() {
-    var container = $('#svg');
+define(['engine', 'jquery', 'elements'], function(Engine, $) {
+    'use strict';
 
-    var controller = {
-        'serverUrl': 'Server',
-        'getTile': function(data) {
-            var tile = (data.content % 2)
-                    ? new Sprite(data.width, data.height, 'Images/grass.png')
-                    : new Tile(data.width, data.height, 'Tile');
+    function Client() {
+        $(document).ready(function() {
+            var container = $('#svg');
 
-            tile.onclick = this.onClick;
+            var controller = {
+                'serverUrl': 'Server',
+                'getTile': function(data) {
+                    var tile = (data.content % 2)
+                            ? new Sprite(data.width, data.height, 'Images/grass.png')
+                            : new Tile(data.width, data.height, 'Tile');
 
-            return tile;
-        },
-        'onClick': function(e) {
-            var tile = e.originalTarget;
+                    tile.onclick = this.onClick;
 
-            var flag = new Polygon([new Point(0, 0), new Point(0, 20), new Point(10, 15), new Point(0, 10)], 'Flag');
+                    return tile;
+                },
+                'onClick': function(e) {
+                    var tile = e.originalTarget;
 
-            // Draw the flag
-            var center = tile.center || tile.wrapper.center;
-            console.log('Placing a flag to ' + center + '.');
-            flag.onclick = function(e) {
-                $(e.originalTarget).remove();
+                    var flag = new Polygon([new Point(0, 0), new Point(0, 20), new Point(10, 15), new Point(0, 10)], 'Flag');
+
+                    // Draw the flag
+                    var center = tile.center || tile.wrapper.center;
+                    console.log('Placing a flag to ' + center + '.');
+                    flag.onclick = function(e) {
+                        $(e.originalTarget).remove();
+                    };
+                    flag.place(center);
+
+                    container.append(flag.svg);
+
+                    // TODO: actually on mouse down place there some temporary item, on mouse up make it permanent and meanwhile ask server whether it can be there and if so, notify it,
+                }
             };
-            flag.place(center);
 
-            container.append(flag.svg);
-
-            // TODO: actually on mouse down place there some temporary item, on mouse up make it permanent and meanwhile ask server whether it can be there and if so, notify it,
-        }
+            var engine = new Engine(container, controller, 'Server');
+            engine.run();
+        });
     };
-
-    var engine = new Engine(container, controller, 'Server');
-    engine.run();
+    return Client;
 });
--- src/main/webapp/Scripts/engine.js
+++ src/main/webapp/Scripts/engine.js
@@ -1,89 +1,13 @@
-/*
- *  Object definitions
- */
-function Point(x, y) {
-    this.x = x;
-    this.y = y;
-}
-
-Point.prototype.toString = function() {
-    return this.x + ',' + this.y;
-};
-
-function Polygon(points, cssClass) {
-    this.points = points;
-    this.cssClass = cssClass;
-}
-
-Polygon.prototype.place = function(target) {
-    // Translate logical points to isometric coordinates and move them to desired location.
-    this.points.forEach(function(point) {
-        point.x = point.x * 1 + target.x;
-        point.y = point.y * -1 + target.y;
-    });
-
-    // Create actual SVG element and append it to the container.
-    var element = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
-    element.setAttribute('points', this.points.join(' '));
-    element.setAttribute('class', this.cssClass);
-    element.wrapper = this;
-    this.svg = element;
+define(['jquery', 'elements'], function($) {
+    'use strict';
     
-    var tooltip = document.createElementNS('http://www.w3.org/2000/svg', 'title');
-    tooltip.innerHTML = this.coordinates;
-    element.appendChild(tooltip);
-
-    element.onclick = function(e) {
-        var handler = e.originalTarget.wrapper.onclick;
-        if (handler) {
-            handler(e);
-        }
-    };
-};
-
-function Tile(width, height, cssClass) {
-    var horizontalRadius = width / 2;
-    var verticalRadius = height / 2;
-
-    this.points = [new Point(0, -verticalRadius), new Point(horizontalRadius, 0), new Point(0, verticalRadius), , new Point(-horizontalRadius, 0)];
-    this.cssClass = cssClass;
-}
-
-// Tile inherits Polygon
-Tile.prototype = new Polygon();
-
-function Sprite(width, height, imageUrl) {
-    this.width = width;
-    this.height = height;
-    this.imageUrl = imageUrl;
-}
-
-Sprite.prototype.place = function(target) {
-    var sprite = document.createElementNS('http://www.w3.org/2000/svg', 'image');
-    sprite.setAttribute('width', this.width);
-    sprite.setAttribute('height', this.height);
-    sprite.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', this.imageUrl);
-    sprite.setAttribute('x', target.x - this.width / 2);
-    sprite.setAttribute('y', target.y - this.height / 2);
-    sprite.setAttribute('class', 'ImageTile')
-    sprite.wrapper = this;
-    this.svg = sprite;
-
-    sprite.onclick = function(e) {
-        var handler = e.originalTarget.wrapper.onclick;
-        if (handler) {
-            handler(e);
-        }
-    };
-};
-
-function Engine(container, controller, serverUrl, refreshSpeed) {
-    this.refreshSpeed = refreshSpeed || 1000;
-    this.container = container;
-    this.controller = controller;
-    this.serverUrl = serverUrl;
-
-    this.redraw = function(data) {
+    function Engine(container, controller, serverUrl, refreshSpeed) {
+        this.refreshSpeed = refreshSpeed || 1000;
+        this.container = container;
+        this.controller = controller;
+        this.serverUrl = serverUrl;
+    }
+    Engine.prototype.redraw = function(data) {
         var dimX = data.map.length;
         var dimY = data.map[0].length;
 
@@ -93,7 +17,7 @@ function Engine(container, controller, serverUrl, refreshSpeed) {
         $('#svg > polygon').remove();
         $('#svg > image').remove();
 
-        var box = container[0].getBBox();
+        var box = this.container[0].getBBox();
         var width = box.width / dimX;
         var height = box.height / dimY;
 
@@ -106,33 +30,31 @@ function Engine(container, controller, serverUrl, refreshSpeed) {
                     'width': width,
                     'height': height
                 };
-                var tile = controller.getTile(tileData);
+                var tile = this.controller.getTile(tileData);
 
                 // Information that general engine needs to track about every tile.			
                 tile.coordinates = new Point(x, y);
 
                 // Position new element when it's finished;
                 tile.center = new Point((x - y) * (width / 2) + halfScreenWidth, (x + y + 1) * (height / 2));
-                
+
                 tile.place(tile.center);
 
-                container.append(tile.svg);
+                this.container.append(tile.svg);
             }
         }
     };
-
-    this.updateAsync = function() {
+    Engine.prototype.updateAsync = function() {
         var xmlHttp = new XMLHttpRequest();
 
         xmlHttp.onreadystatechange = (function(that) {
             return function() {
-                if ((this.readyState == 4) && (this.status == 200)) {
+                if ((this.readyState === 4) && (this.status === 200)) {
                     var data = JSON.parse(this.responseText);
                     that.redraw(data);
                 }
             };
         })(this);
-
 
         try {
             xmlHttp.open("GET", this.serverUrl, true);
@@ -143,8 +65,7 @@ function Engine(container, controller, serverUrl, refreshSpeed) {
             console.log(error);
         }
     };
-
-    this.run = function() {
+    Engine.prototype.run = function() {
         // TODO: Make this a timer when rendering is optimized enough.
         this.timer = setTimeout((function(that) {
             return function() {
@@ -152,4 +73,6 @@ function Engine(container, controller, serverUrl, refreshSpeed) {
             };
         })(this), this.refreshSpeed);
     };
-}
+
+    return Engine;
+});
